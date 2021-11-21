c59c3c3c0ac389e8537870c5146311168a627082
CarolineCos <carleycos@gmail.com>
2021-11-21 15:21:27 +0000

    testing random selection in get_word function

--- run.py
+++ run.py
@@ -1,2 +1,20 @@
 import random
-import words from words
+from words import words
+
+def get_word(words):
+    word = random.choice(words)
+    while "-" in word or " " in word:
+        word = random.choice(words)
+    
+    return word.upper()
+
+word = get_word(words)
+print(word)
+"""
+def play(word):
+    word_completion = "_" * len(word)
+    guess = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 7
+"""
